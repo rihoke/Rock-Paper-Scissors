25b91c8af4b2b779e8df5232e1766faa5f202e28
rihoke <leopardriho22@gmail.com>
2026-01-25 22:11:29 +0200

    made the human choice function, made it case-insensitive and started the logic behind a single round

--- javascript.js
+++ javascript.js
@@ -4,28 +4,51 @@ function getComputerChoice(){
     function getRandomInt(max){
         return Math.floor(Math.random() * max);
     }
-    console.log(getRandomInt(3))
+    // console.log(getRandomInt(3));
 
-    for (let i = getRandomInt; i <=3; i++) {
+    for (let i = getRandomInt(3); i <=3; i++) {
         if (i === 0) {
-            return rock
+            return "rock"
         }
         else if (i === 1) {
-            return paper
+            return "paper"
         }
         else if (i === 2) {
-            return scissors
+            return "scissors"
         }
         else {
             return "Incorrect input"
         }
     }
-
-
-    // const 0 ="rock";
-    // const 1 ="paper";
-    // const 2 ="scissors";
+    
     
 }
 
-console.log(getComputerChoice());
+console.log(getComputerChoice())
+
+function getHumanChoice(){
+
+    let choice = prompt("rock, paper or scissors?");
+
+    return choice.toLowerCase();
+
+}
+
+console.log(getHumanChoice());
+
+let humanScore = "0";
+
+let computerScore = "0";
+
+
+function playRound(computerChoice, humanChoice) {
+
+
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
